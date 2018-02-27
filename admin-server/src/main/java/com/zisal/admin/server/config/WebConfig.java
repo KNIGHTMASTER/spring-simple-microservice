9dd5d475e0c9aa52770e186d31e95d2f51000647
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2018-02-27 13:06:51 +0700

    Fixed spring boot admin UI

--- admin-server/src/main/java/com/zisal/admin/server/config/WebConfig.java
+++ admin-server/src/main/java/com/zisal/admin/server/config/WebConfig.java
@@ -1,8 +1,8 @@
 package com.zisal.admin.server.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+/*import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;*/
 
 /**
  * Created on 2/26/18.
@@ -10,29 +10,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @author <a href="mailto:fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
  */
 @Configuration
-public class WebConfig extends WebSecurityConfigurerAdapter {
+public class WebConfig /*extends WebSecurityConfigurerAdapter */{
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/login")
-                .permitAll();
-        http
-                .logout()
-                .logoutUrl("/logout");
-        http
-                .csrf()
-                .disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-                .permitAll();
-        http
-                .authorizeRequests()
-                .antMatchers("/**")
-                .authenticated();
-        http.httpBasic();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .formLogin()
+//                .loginPage("/login.html")
+//                .loginProcessingUrl("/login")
+//                .permitAll();
+//        http
+//                .logout()
+//                .logoutUrl("/logout");
+//        http
+//                .csrf()
+//                .disable();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+//                .permitAll();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/**")
+//                .authenticated();
+//        http.httpBasic();
+//    }
 }
